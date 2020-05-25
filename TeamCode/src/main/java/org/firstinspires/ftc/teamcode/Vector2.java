48430e642ff28a6c445b53b4d6c266901e0a3330
920780950@qq.com <sb945594sb>
2020-05-25 21:52:41 +0800

    给二维向量类加了些注释

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vector2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vector2.java
@@ -10,76 +10,143 @@ public class Vector2 {
         this.y = y;
     }
 
-    public String getString() {
-        return Double.toString(x) + ", " + Double.toString(y);
-    }
-
+    /**
+     * 获得x值
+     * @return x值
+     */
     public double getX() {
         return x;
     }
+
+    /**
+     * 设置x值
+     * @param x x值
+     */
     public void setX(double x) {
         this.x = x;
     }
-    public double getY() { return y; }
+
+    /**
+     * 获得y值
+     * @return y值
+     */
+    public double getY() {
+        return y;
+    }
+
+    /**
+     * 设置y值
+     * @param y y值
+     */
     public void setY(double y) {
         this.y = y;
     }
 
+    /**
+     * 设置x、y值
+     * @param x x值
+     * @param y y值
+     */
     public void set(double x, double y){
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * 获取向量旋转一定度数后的向量
+     * @param deg 旋转读数
+     * @return 旋转后的向量值
+     */
     public Vector2 rotated(double deg){
         double x1 = x * Math.cos(Math.toRadians(deg)) - y * Math.sin(Math.toRadians(deg));
         double y1 = y * Math.cos(Math.toRadians(deg)) + x * Math.sin(Math.toRadians(deg));
         return new Vector2(x1, y1);
     }
 
+    /**
+     * 向量归一化
+     * @return 获取向量归一化后的向量
+     */
     public Vector2 normalized(){
         double length = length();
         return new Vector2(x / length, y / length);
     }
 
+    /**
+     * 获取向量长度
+     * @return 向量长度
+     */
     public double length(){
         return Math.sqrt(x * x + y * y);
     }
 
+    /**
+     * 获得与目标向量相加后的向量
+     * @param target 目标向量
+     */
     public void plus(Vector2 target){
         this.x += target.getX();
         this.y += target.getY();
     }
 
+    /**
+     * 获得与目标向量相减后的向量
+     * @param target 目标向量
+     */
     public void minus(Vector2 target){
         this.x -= target.getX();
         this.y -= target.getY();
     }
 
+    /**
+     * 获得与目标数相乘后的向量
+     * @param target 目标数值
+     */
     public void multiply(int target){
         this.x *= target;
         this.y *= target;
     }
 
+    /**
+     * 获得与目标数相乘后的向量
+     * @param target 目标数值
+     */
     public void multiply(float target){
         this.x *= target;
         this.y *= target;
     }
 
+    /**
+     * 获得与目标数相乘后的向量
+     * @param target 目标数值
+     */
     public void multiply(double target){
         this.x *= target;
         this.y *= target;
     }
 
+    /**
+     * 获得与目标数相除后的向量
+     * @param target 目标数值
+     */
     public void devide(int target){
         this.x /= target;
         this.y /= target;
     }
 
+    /**
+     * 获得与目标数相除后的向量
+     * @param target 目标数值
+     */
     public void devide(float target){
         this.x /= target;
         this.y /= target;
     }
 
+    /**
+     * 获得与目标数相除后的向量
+     * @param target 目标数值
+     */
     public void devide(double target){
         this.x /= target;
         this.y /= target;
